a5eba0d90e2c6987fa8135c7e9bb89de0a739432
BAKKIBABU <108388125+BAKKIBABU@users.noreply.github.com>
2022-06-29 11:32:43 +0530

    Add files via upload

---
+++ print statement.py
@@ -0,0 +1,30 @@
+# # print('hi babu')
+# # print('hi shravan')
+# # print('hi prem sagaer')
+# # print("hi swamy")
+# # print("hi praneeth")
+# # print('What Can You Do With Our Small Text Generator?')
+# # print('''What Can You Do With Our Small Text Generator?''')c
+# # print("""What Can You Do With Our 
+# #       Small Text Generator?""")
+# # print("""What Can
+# #       You Do With Our Small Text Generator?""")
+# a=1,2,3
+# b=1,2,3
+# print(a+b)
+# a=1+2+3+4
+# b=1+2+3+4
+# print(a+b)
+# print('hi babu\n hi chaithanya\n hi sai kumar\n hi ravi kumar')
+# print('''hi babu\n\n hi chaithanya\n hi sai kumar\n hi ravi kumar''')
+# print('''hi babu\n\n hi chaithanya\n hi sai kumar\n hi ravi kumar''')
+# print('''hi babu\n\n hi chaithanya\n hi sai kumar\n hi ravi kumar''')
+# print("""hi babu\n\n hi chaithanya\n hi sai kumar\n hi ravi kumar""")
+# print("""hi babu\n\n hi chaithanya\n hi sai kumar\n hi ravi kumar""")
+# print("""hi babu\n\n hi chaithanya\n hi sai kumar\n hi ravi kumar""")
+# z=5
+# k=8
+# print(z+k)
+# print('Welcome! This website is (quite obviously) a small text generator')
+
+
